59c2105a642528b641e4f3b38d0125068e23495a
CarterH93 <Carterhawkins93@gmail.com>
2022-12-17 17:17:19 -0500

    Finished Start menu

--- ScaleMaster/ContentView.swift
+++ ScaleMaster/ContentView.swift
@@ -8,14 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tubaRotate = 0.0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            GeometryReader { geo in
+                    ZStack {
+                        LinearGradient(gradient: Gradient(colors: [Color("NiceOrange"), .white, .gray]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            .ignoresSafeArea()
+                        
+                        Image("Music Scales")
+                            .resizable()
+                            .opacity(0.04)
+                        
+                        
+                    
+                    VStack(spacing: 15) {
+                        Text("Scale Master 🎶")
+                            .fontWeight(.semibold)
+                            .font(.system(size: geo.size.height > geo.size.width ? geo.size.width * 0.1: geo.size.height * 0.1))
+                            .padding(5)
+                            .foregroundColor(.black)
+                        
+                            .background(.quaternary)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                            .padding(.top)
+                        
+                        Image("Tuba")
+                            .resizable()
+                            .scaledToFit()
+                            .rotation3DEffect(.degrees(tubaRotate), axis: (x: 0, y: 1, z: 0))
+                            .onTapGesture {
+                                withAnimation(.interpolatingSpring(stiffness: 60, damping: 6)) {
+                                    tubaRotate += 360
+                                }
+                            }
+                        Button {
+                            
+                        } label: {
+                            
+                            
+                            Text("Start")
+                                .font(.system(size: geo.size.height > geo.size.width ? geo.size.width * 0.07: geo.size.height * 0.07))
+                                .padding(15)
+                                .background(.quaternary)
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                        }
+                        Button {
+                            
+                        } label: {
+                            
+                            
+                            Text("Settings")
+                                .font(.system(size: geo.size.height > geo.size.width ? geo.size.width * 0.07: geo.size.height * 0.07))
+                                .foregroundColor(.secondary)
+                                .padding(8)
+                                .background(.thinMaterial)
+                                .clipShape(Rectangle())
+                                .border(.black.opacity(0.5))
+                            
+                        }
+                        
+                    }
+                    .toolbar {
+                        ToolbarItem(placement: .bottomBar) {
+                            
+                            Spacer()
+                        }
+                        ToolbarItem(placement: .bottomBar) {
+                            
+                            Text("Created by Carter Hawkins")
+                                .foregroundColor(.accentColor)
+                               
+                        }
+                    }
+                            
+                }
+            }
         }
-        .padding()
     }
 }
 
