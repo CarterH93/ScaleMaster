9985c4b5fa809c59cacdd17d406278048dede64a
CarterH93 <Carterhawkins93@gmail.com>
2022-12-17 14:31:56 -0500

    Initial Commit

---
+++ ScaleMaster/ScaleMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleMasterApp.swift
+//  ScaleMaster
+//
+//  Created by Carter Hawkins on 12/17/22.
+//
+
+import SwiftUI
+
+@main
+struct ScaleMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
