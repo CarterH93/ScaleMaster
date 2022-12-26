8ffffa5e068bf79c15ce0da192c8011c4bb1190c
CarterH93 <Carterhawkins93@gmail.com>
2022-12-25 21:35:32 -0500

    checkpoint

--- ScaleMaster/ScaleWithMarking.swift
+++ ScaleMaster/ScaleWithMarking.swift
@@ -19,7 +19,7 @@ struct ScaleWithMarking: View {
     var getYPosition: Double {
         if scale.octaves == 0 {
             
-            if position > 30 {
+            if position > 29 {
                 return 0.3
             } else {
                 return -0.3
@@ -151,6 +151,115 @@ struct ScaleWithMarking: View {
                             return 3.6
                         case 7:
                             return 3.3
+                        case 8:
+                            return 3.05
+                        case 9:
+                            return 2.87
+            case 10:
+                return 2.7
+            case 11:
+                return 2.55
+            case 12:
+                return 2.32
+            case 13:
+                return 2.2
+            case 14:
+                return 2.13
+            case 15:
+                return 2.03
+            case 16:
+                return 1.95
+            case 17:
+                return 1.88
+            case 18:
+                return 1.74
+            case 19:
+                return 1.65
+            case 20:
+                return 1.6
+            case 21:
+                return 1.55
+            case 22:
+                return 1.5
+            case 23:
+                return 1.45
+            case 24:
+                return 1.41
+            case 25:
+                return 1.38
+            case 26:
+                return 1.33
+            case 27:
+                return 1.27
+            case 28:
+                return 1.24
+            case 29:
+                return 1.21
+            case 30:
+                return 6.0
+            case 31:
+                return 5.2
+            case 32:
+                return 4.7
+            case 33:
+                return 4.3
+            case 34:
+                return 3.9
+            case 35:
+                return 3.6
+            case 36:
+                return 3.4
+            case 37:
+                return 3.1
+            case 38:
+                return 2.9
+            case 39:
+                return 2.7
+            case 40:
+                return 2.55
+            case 41:
+                return 2.43
+            case 42:
+                return 2.3
+            case 43:
+                return 2.2
+            case 44:
+                return 2.1
+            case 45:
+                return 2.0
+            case 46:
+                return 1.88
+            case 47:
+                return 1.85
+            case 48:
+                return 1.8
+            case 49:
+                return 1.74
+            case 50:
+                return 1.67
+            case 51:
+                return 1.65
+            case 52:
+                return 1.6
+            case 53:
+                return 1.56
+            case 54:
+                return 1.5
+            case 55:
+                return 1.45
+            case 56:
+                return 1.38
+            case 57:
+                return 1.35
+            case 58:
+                return 1.305
+            case 59:
+                return 1.27
+            case 60:
+                return 1.232
+            case 61:
+                return 1.195
+                
                             
                         default:
                             return 9.4
@@ -191,7 +300,7 @@ struct ScaleWithMarking: View {
 
 struct ScaleWithMarking_Previews: PreviewProvider {
     static var previews: some View {
-        ScaleWithMarking(scale: AppInfoStorage.allScales[12], showingFingeringsImage: true, position: 1)
+        ScaleWithMarking(scale: AppInfoStorage.allScales[12], showingFingeringsImage: true, position: 61)
             .environmentObject(AppInfoStorage())
     }
 }
